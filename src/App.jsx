008d75b4cb2b0270ff0211aaa27d9d4ccc186199
liviuschera <liviu.schera@gmail.com>
2024-02-18 13:52:40 +0200

    add styled-component dependency and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    color: turquoise;
+`;
+
 function App() {
-    return <div>Hello Gym Pass</div>;
+    return (
+        <div>
+            <H1>Gym Pass</H1>
+        </div>
+    );
 }
 
 export default App;
