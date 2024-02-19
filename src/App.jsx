e8d0d1d3f405f93dbbae08c9bac39b328b22f247
liviuschera <liviu.schera@gmail.com>
2024-02-19 18:12:51 +0200

    add AppLayout Header and Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
 import PageNotFound from "./pages/PageNotFound";
 import Account from "./pages/Account";
 import Bookings from "./pages/Bookings";
@@ -6,21 +7,31 @@ import Classes from "./pages/Classes";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Members from "./pages/Members";
+import { Navigate } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
     return (
-        <BrowserRouter>
-            <h1>App</h1>
-            <Routes>
-                <Route path="/account" element={<Account />} />
-                <Route path="/bookings" element={<Bookings />} />
-                <Route path="/classes" element={<Classes />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/members" element={<Members />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route
+                            index
+                            element={<Navigate replace to="/dashboard" />}
+                        />
+                        <Route path="/account" element={<Account />} />
+                        <Route path="/bookings" element={<Bookings />} />
+                        <Route path="/classes" element={<Classes />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/members" element={<Members />} />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
