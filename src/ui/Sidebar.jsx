ed67eb1f1abd3e70533c514c7e5291ab0db2773e
liviuschera <liviu.schera@gmail.com>
2024-02-19 21:46:20 +0200

    add cusstom colors inspired from tailwind css in globalstyles

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 const StyledSidebar = styled.aside`
-    /* grid-column: 1; */
     grid-row: 1/-1;
-    background-color: yellowgreen;
+    background-color: var(--color-grey-0);
     padding: 3.2rem 2.4rem;
-    border-right: 1px solid grey;
+    border-right: 1px solid var(--color-grey-100);
 `;
 
 function Sidebar() {
