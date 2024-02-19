b5806e5dc74616220501852c3d6e9d6befb56074
liviuschera <liviu.schera@gmail.com>
2024-02-19 20:35:27 +0200

    add grid to app layout

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    background-color: wheat;
+    padding: 1.2rem 4.8rem;
+    border-bottom: 1px solid grey;
+`;
+
 function Header() {
-    return <header>Header</header>;
+    return <StyledHeader>StyledHeader</StyledHeader>;
 }
 
 export default Header;
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+    /* grid-column: 1; */
+    grid-row: 1/-1;
+    background-color: yellowgreen;
+    padding: 3.2rem 2.4rem;
+    border-right: 1px solid grey;
+`;
+
 function Sidebar() {
-    return <aside>Sidebar</aside>;
+    return <StyledSidebar>Sidebar</StyledSidebar>;
 }
 
 export default Sidebar;
