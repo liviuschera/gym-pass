ad6db0b529e63aa013f5129aa2ff3665350c99ba
liviuschera <liviu.schera@gmail.com>
2024-03-02 06:46:13 +0200

    add aActivityRow component and helpers functions file

--- src/components/activities/ActivityTable.jsx
+++ src/components/activities/ActivityTable.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { getActivities } from "../../services/getActivities";
 import Spinner from "../../ui/Spinner";
+import ActivityRow from "./ActivityRow";
 
 const Table = styled.div`
     border: 1px solid var(--color-grey-200);
@@ -10,11 +11,12 @@ const Table = styled.div`
     background-color: var(--color-grey-0);
     border-radius: 7px;
     overflow: hidden;
+    font-family: "M PLUS Rounded 1c";
 `;
 
 const TableHeader = styled.header`
     display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 0.6fr 1.8fr 2fr 1fr 1fr 1fr 1fr;
     column-gap: 2.4rem;
     align-items: center;
 
@@ -30,7 +32,7 @@ const TableHeader = styled.header`
 function ActivityTable() {
     const {
         isLoading,
-        data: activity,
+        data: activities,
         error,
     } = useQuery({
         queryKey: ["activities"],
@@ -45,12 +47,16 @@ function ActivityTable() {
             <TableHeader role="row">
                 <div>Image</div>
                 <div>Name</div>
-                <div>Description</div>
+                {/* <div>Description</div> */}
                 <div>Type</div>
                 <div>Max Capacity</div>
                 <div>Regular Price</div>
                 <div>Discount</div>
+                <div>Delete</div>
             </TableHeader>
+            {activities.map((activity) => (
+                <ActivityRow key={activity.id} activity={activity} />
+            ))}
         </Table>
     );
 }
