e449052735f544b46fbff48ec657caacbb8c28a8
liviuschera <liviu.schera@gmail.com>
2024-03-05 20:25:48 +0200

    fix AppLayout

--- src/components/activities/ActivityTable.jsx
+++ src/components/activities/ActivityTable.jsx
@@ -6,7 +6,6 @@ import ActivityRow from "./ActivityRow";
 
 const Table = styled.div`
     border: 1px solid var(--color-grey-200);
-
     font-size: 1.4rem;
     background-color: var(--color-grey-0);
     border-radius: 7px;
