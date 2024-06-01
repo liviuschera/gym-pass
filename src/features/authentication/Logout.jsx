be5a0b369747fb4f188bb753eb30edd5259651c6
liviuschera <liviu.schera@gmail.com>
2024-06-01 07:24:33 +0300

    implement logout

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,14 @@
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function Logout() {
+    const { logout, isPending } = useLogout();
+
     return (
-        <ButtonIcon>
-            <HiArrowRightOnRectangle />
+        <ButtonIcon onClick={logout} disabled={isPending}>
+            {!isPending ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
         </ButtonIcon>
     );
 }
--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,1 +1,23 @@
-export
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { logout as logoutApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+
+export function useLogout() {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+
+    const { mutate: logout, isPending } = useMutation({
+        mutationFn: () => logoutApi(),
+        onSuccess: () => {
+            queryClient.removeQueries();
+            navigate("/login", { replace: true });
+        },
+        onError: (error) => {
+            console.log("🚀 ~ useLogout ~ error:", error.message);
+            toast.error(error.message);
+        },
+    });
+
+    return { logout, isPending };
+}
