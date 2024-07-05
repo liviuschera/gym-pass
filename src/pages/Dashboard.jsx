4725e9df77b3da062351e97dac33a20f4a08ed28
liviuschera <liviu.schera@gmail.com>
2024-07-05 13:28:58 +0300

    add Daashboard layout and filter

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,18 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import DashboardLayout from "../dashboard/DashboardLayout";
+import DashboardFilter from "../dashboard/DashboardFilter";
+
 function Dashboard() {
-    return <div>Dashboard</div>;
+    return (
+        <>
+            <Row>
+                <Heading as="h1">Dashboard</Heading>
+                <DashboardFilter />
+            </Row>
+            <DashboardLayout />
+        </>
+    );
 }
 
 export default Dashboard;
